85bebc06f1c56043802eca5a1387a0fcc5d86883
Andrea Lai <laidevpub@gmail.com>
2017-07-16 18:46:46 +0200

    modifica legenda conteneva apici stringa - problemi json

--- Main.elm
+++ Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Json.Decode exposing (..)
 import Json.Decode.Pipeline exposing (..)
 import LegendaUno
@@ -8,40 +9,47 @@ import LegendaUno
 
 -- MODEL
 
+
 test : List ItemLegenda
 test =
-  [ { liv_2 = "a", liv_2_desc = "prova", rgb = "#ff0000" }
-  , { liv_2 = "b", liv_2_desc = "prova", rgb = "#ff0000" } ]
+    [ { liv_2 = "a", liv_2_desc = "prova", rgb = "#ff0000" }
+    , { liv_2 = "b", liv_2_desc = "prova", rgb = "#ff0000" }
+    ]
+
 
 my_error : List ItemLegenda
 my_error =
-  [ { liv_2 = "e1", liv_2_desc = "prova", rgb = "#ff0000" }
-  , { liv_2 = "e2", liv_2_desc = "prova", rgb = "#ff0000" } ]
-
+    [ { liv_2 = "e1", liv_2_desc = "prova", rgb = "#ff0000" }
+    , { liv_2 = "e2", liv_2_desc = "prova", rgb = "#ff0000" }
+    ]
 
 
 itemLegendaDecoder : Decoder ItemLegenda
 itemLegendaDecoder =
     decode ItemLegenda
-        |> required "liv_2" string
-        |> required "liv_2_desc" string
-        |> required "rgb" string
+        |> Json.Decode.Pipeline.required "liv_2" string
+        |> Json.Decode.Pipeline.required "liv_2_desc" string
+        |> Json.Decode.Pipeline.required "rgb" string
 
 
 legendaDecoder : Decoder (List ItemLegenda)
 legendaDecoder =
     decode identity
-        |> required "data" (list itemLegendaDecoder)
+        |> Json.Decode.Pipeline.required "data" (Json.Decode.list itemLegendaDecoder)
 
 
 decodeLegenda : String -> List ItemLegenda
 decodeLegenda json =
     case decodeString legendaDecoder json of
         Ok itemsLegenda ->
-            test
+            itemsLegenda
 
-        _ ->
-            my_error
+        Err errore ->
+            let
+                _ =
+                    Debug.log errore []
+            in
+            []
 
 
 model : Model
@@ -83,9 +91,37 @@ update msg model =
 -- VIEW
 
 
+(=>) =
+    (,)
+
+
+px : Int -> String
+px number =
+    toString number ++ "px"
+
+
 view : Model -> Html Msg
 view model =
-    ul [] (List.map (\item -> li [] [ text item.liv_2 ]) model.legenda)
+    ul []
+        (List.map
+            (\item ->
+                div
+                    [ style
+                        [ "background-color" => item.rgb
+                        , "width" => "auto"
+                        , "height" => "20px"
+                        , "border-radius" => "4px"
+                        , "color" => "black"
+                        , "display" => "flex"
+                        ]
+                    ]
+                    [ li
+                        []
+                        [ text item.liv_2, text " - ", text item.liv_2_desc ]
+                    ]
+            )
+            model.legenda
+        )
 
 
 main : Program Never Model Msg
